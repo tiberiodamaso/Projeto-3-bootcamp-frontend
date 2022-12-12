b832b81ac70e5f3c03c0c4430d5bae93e582ce8a
Marcos Royer <marcosoliveiraroyer@gmail.com>
2022-12-12 17:36:55 -0300

    adiciona calculos - refatorado

--- src/pages/Analises.js
+++ src/pages/Analises.js
@@ -61,19 +61,25 @@ function Analises() {
 
   //
   async function handleClick(ano, trimestre) {
-    let dcpSection = document.querySelector('#dcpSection')
-    let dcpMetaData = document.querySelector('#dcpMetaData')
-    const cnpjLimpo = cnpj.replace(/\D/g, "")
-    const response = await api.get(`/dcp/all-dcp?cnpj=${cnpjLimpo}&ano=${ano}&trimestre=${trimestre}`)
-    setDcp1(response.data[0])
-    setDcp2(response.data[1])
-    setDcp3(response.data[2])
-    dcpSection.classList.toggle('d-none')
-    dcpMetaData.innerText = `${trimestre}/${ano}`
-    setIsLoading(false)
+    let dcpSection = document.querySelector("#dcpSection");
+    let dcpMetaData = document.querySelector("#dcpMetaData");
+    const cnpjLimpo = cnpj.replace(/\D/g, "");
+    const response = await api.get(
+      `/dcp/all-dcp?cnpj=${cnpjLimpo}&ano=${ano}&trimestre=${trimestre}`
+    );
+    setDcp1(response.data[0]);
+    setDcp2(response.data[1]);
+    setDcp3(response.data[2]);
+    dcpSection.classList.toggle("d-none");
+    dcpMetaData.innerText = `${trimestre}/${ano}`;
+
+    const resposta = await api.get("/nfe/all-nfe", {
+      params: { cnpj: cnpjLimpo, trim: trimestre, ano: ano },
+    });
+    setGomoExport(exportacoes(resposta.data, ano, trimestre));
+
+    setIsLoading(false);
   }
-
-
 
   return (
     <div className="d-flex">
@@ -130,16 +136,23 @@ function Analises() {
       {/* DCP E ANÁLISES */}
       <div className="container mx-5">
         {/* DCP */}
-        <div className='d-none' id='dcpSection'>
-          <div className='d-flex justify-content-between'>
-            <h2 className='py-5 mx-3'>Demonstrativo de Crédito Presumido</h2>
-            <h2 className='py-5 mx-3' id='dcpMetaData'></h2>
+        <div className="d-none" id="dcpSection">
+          <div className="d-flex justify-content-between">
+            <h2 className="py-5 mx-3">Demonstrativo de Crédito Presumido</h2>
+            <h2 className="py-5 mx-3" id="dcpMetaData"></h2>
           </div>
           <div className="accordion mx-3" id="accordionExample">
             <div className="accordion-item">
               <h2 className="accordion-header" id="headingOne">
-                <button className="accordion-button fs-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                  <span className='col-8'>Exportação direta no mês</span>
+                <button
+                  className="accordion-button fs-4"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseOne"
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  <span className="col-8">Exportação direta no mês</span>
                 </button>
               </h2>
               <div
@@ -169,70 +182,174 @@ function Analises() {
                       </tr>
                     </thead>
                     {!isLoading && (
-
-                      <tbody className='table-group-divider'>
+                      <tbody className="table-group-divider">
                         <tr>
                           <td>Linha 1</td>
-                          <td>{dcp1.linha_1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp2.linha_1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp3.linha_1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
+                          <td>
+                            {dcp1.linha_1.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[0].linha_1}</td>
+                          <td>
+                            {dcp2.linha_1.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[1].linha_1}</td>
+                          <td>
+                            {dcp3.linha_1.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[2].linha_1}</td>
                         </tr>
                         <tr>
                           <td>Linha 2</td>
-                          <td>{dcp1.linha_2.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp2.linha_2.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp3.linha_2.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
+                          <td>
+                            {dcp1.linha_2.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[0].linha_2}</td>
+                          <td>
+                            {dcp2.linha_2.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[1].linha_}2</td>
+                          <td>
+                            {dcp3.linha_2.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[2].linha_2}</td>
                         </tr>
                         <tr>
                           <td>Linha 3</td>
-                          <td>{dcp1.linha_3.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp2.linha_3.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp3.linha_3.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
+                          <td>
+                            {dcp1.linha_3.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[0].linha_3}</td>
+                          <td>
+                            {dcp2.linha_3.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[1].linha_3}</td>
+                          <td>
+                            {dcp3.linha_3.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[2].linha_3}</td>
                         </tr>
                         <tr>
                           <td>Linha 4</td>
-                          <td>{dcp1.linha_4.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp2.linha_4.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp3.linha_4.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
+                          <td>
+                            {dcp1.linha_4.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[0].linha_4}</td>
+                          <td>
+                            {dcp2.linha_4.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[1].linha_4}</td>
+                          <td>
+                            {dcp3.linha_4.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[2].linha_4}</td>
                         </tr>
                         <tr>
                           <td>Linha 5</td>
-                          <td>{dcp1.linha_5.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp2.linha_5.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp3.linha_5.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
+                          <td>
+                            {dcp1.linha_5.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[0].linha_5}</td>
+                          <td>
+                            {dcp2.linha_5.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[1].linha_5}</td>
+                          <td>
+                            {dcp3.linha_5.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[2].linha_5}</td>
                         </tr>
                         <tr>
                           <td>Linha 6</td>
-                          <td>{dcp1.linha_6.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp2.linha_6.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp3.linha_6.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
+                          <td>
+                            {dcp1.linha_6.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[0].linha_6}</td>
+                          <td>
+                            {dcp2.linha_6.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[1].linha_6}</td>
+                          <td>
+                            {dcp3.linha_6.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[2].linha_6}</td>
                         </tr>
                         <tr>
                           <td>Linha 7</td>
-                          <td>{dcp1.linha_7.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp2.linha_7.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
-                          <td>{dcp3.linha_7.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                          <td></td>
+                          <td>
+                            {dcp1.linha_7.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[0].linha_7}</td>
+                          <td>
+                            {dcp2.linha_7.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[1].linha_7}</td>
+                          <td>
+                            {dcp3.linha_7.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            })}
+                          </td>
+                          <td>{gomoExport[2].linha_7}</td>
                         </tr>
                       </tbody>
                     )}
@@ -242,8 +359,15 @@ function Analises() {
             </div>
             <div className="accordion-item">
               <h2 className="accordion-header" id="headingTwo">
-                <button className="accordion-button fs-4 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  <span className='col-8'>Matérias primas, embalagens</span>
+                <button
+                  className="accordion-button fs-4 collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="collapseTwo"
+                >
+                  <span className="col-8">Matérias primas, embalagens</span>
                 </button>
               </h2>
               <div
@@ -307,8 +431,15 @@ function Analises() {
             </div>
             <div className="accordion-item">
               <h2 className="accordion-header" id="headingThree">
-                <button className="accordion-button fs-4 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                  <span className='col-8'>Prestação de serviços</span>
+                <button
+                  className="accordion-button fs-4 collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseThree"
+                  aria-expanded="false"
+                  aria-controls="collapseThree"
+                >
+                  <span className="col-8">Prestação de serviços</span>
                 </button>
               </h2>
               <div
@@ -383,10 +514,7 @@ function Analises() {
           <button class="btn btn-primary mx-3">Salvar análise</button>
           <button class="btn btn-primary">Gerar relattório</button>
         </div>
-
       </div>
-    </div>
-
     </div>
   );
 }
