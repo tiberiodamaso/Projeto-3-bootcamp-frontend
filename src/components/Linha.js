757caa2269dff38e7554f1651447fee7db239448
Marcos Royer <marcosoliveiraroyer@gmail.com>
2022-12-13 21:33:05 -0300

    adiciona gomos de energia e combustivel

--- src/components/Linha.js
+++ src/components/Linha.js
@@ -1,56 +1,100 @@
 import { useNavigate } from "react-router-dom";
 
 function LinhaDCP({ dcpsTrimestre, gomo, nLinha }) {
-  
-  const descricoes = require('../utils/descricaolinhas.json');
-  
-  const navigate = useNavigate()
-  const clicavel = [4, 5]
-  const [dcp1, dcp2, dcp3] = dcpsTrimestre
+  const descricoes = require("../utils/descricaolinhas.json");
+
+  const navigate = useNavigate();
+  const clicavel = [4, 5];
+  const [dcp1, dcp2, dcp3] = dcpsTrimestre;
 
   function getNotas(e) {
     // console.log(e)
-    const cnpj = dcp1.cnpj
-    const ano = dcp1.ano
-    const mes = e.target.dataset.mes
+    const cnpj = dcp1.cnpj;
+    const ano = dcp1.ano;
+    const mes = e.target.dataset.mes;
     if (clicavel.includes(nLinha)) {
-      navigate('/notas', {
+      navigate("/notas", {
         state: {
           cnpj: cnpj,
           ano: ano,
           mes: mes,
-          nLinha: nLinha
-        }
-      })
+          nLinha: nLinha,
+        },
+      });
     }
   }
 
   return (
     <tr>
-      <td data-toggle="tooltip" data-placement="right" title={descricoes[nLinha]} >Linha {nLinha}</td>
-      <td>{dcp1[`linha_${nLinha}`].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+      <td
+        data-toggle="tooltip"
+        data-placement="right"
+        title={descricoes[nLinha]}
+      >
+        Linha {nLinha}
+      </td>
+      <td>
+        {dcp1[`linha_${nLinha}`].toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </td>
       <td
         onClick={getNotas}
-        className={`${Math.floor(dcp1[`linha_${nLinha}`]) !== Math.floor(gomo[0][`linha_${nLinha}`]) ? "text-danger" : ""}`}
+        className={`${
+          Math.floor(dcp1[`linha_${nLinha}`]) !==
+          Math.floor(gomo[0][`linha_${nLinha}`])
+            ? "text-danger"
+            : ""
+        }`}
         data-mes={dcp1.mes}
       >
-        {gomo[0][`linha_${nLinha}`].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+        {gomo[0][`linha_${nLinha}`].toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
       </td>
-      <td>{dcp2[`linha_${nLinha}`].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+      <td>
+        {dcp2[`linha_${nLinha}`].toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </td>
       <td
         onClick={getNotas}
-        className={`${Math.floor(dcp2[`linha_${nLinha}`]) !== Math.floor(gomo[1][`linha_${nLinha}`]) ? "text-danger" : ""}`}
+        className={`${
+          Math.floor(dcp2[`linha_${nLinha}`]) !==
+          Math.floor(gomo[1][`linha_${nLinha}`])
+            ? "text-danger"
+            : ""
+        }`}
         data-mes={dcp2.mes}
       >
-        {gomo[1][`linha_${nLinha}`].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+        {gomo[1][`linha_${nLinha}`].toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
       </td>
-      <td>{dcp3[`linha_${nLinha}`].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+      <td>
+        {dcp3[`linha_${nLinha}`].toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </td>
       <td
         onClick={getNotas}
-        className={`${Math.floor(dcp3[`linha_${nLinha}`]) !== Math.floor(gomo[2][`linha_${nLinha}`]) ? "text-danger" : ""}`}
+        className={`${
+          Math.floor(dcp3[`linha_${nLinha}`]) !==
+          Math.floor(gomo[2][`linha_${nLinha}`])
+            ? "text-danger"
+            : ""
+        }`}
         data-mes={dcp3.mes}
       >
-        {gomo[2][`linha_${nLinha}`].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+        {gomo[2][`linha_${nLinha}`].toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
       </td>
     </tr>
   );
@@ -58,16 +102,23 @@ function LinhaDCP({ dcpsTrimestre, gomo, nLinha }) {
 
 function LinhaNota({ nota }) {
   return (
-    <tr style={{verticalAlign: 'middle'}}>
+    <tr style={{ verticalAlign: "middle" }}>
       <td>{nota.operacao}</td>
       <td>{nota.pais}</td>
       <td>{nota.cfop}</td>
       <td>{nota.ncm}</td>
       <td>{nota.mercadoria}</td>
-      <td>{nota.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+      <td>
+        {nota.valor.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </td>
       <td className="col-2">{nota.ni}</td>
       <td>{nota.nome}</td>
-      <td><i className="bi bi-file-earmark-x-fill text-danger"></i></td>
+      <td>
+        <i className="bi bi-file-earmark-x-fill text-danger"></i>
+      </td>
     </tr>
   );
 }
