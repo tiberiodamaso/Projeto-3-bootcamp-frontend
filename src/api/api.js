3dac00fb3f1114a62d5f3ae17f2e01223bfa014a
tiberio <tiberio@gmail.com>
2022-12-15 12:11:27 -0300

    Deploy com banco correto

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:8080",
-  production: "https://analisedcp.netlify.app",
+  production: "https://analisedcp.cyclic.app/",
 };
 
 // Essa instância do axios sabe qual é a baseURL que deve ser usada nas requisições
